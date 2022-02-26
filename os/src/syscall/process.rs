57d86bb3bb657426db47340812922e920e259b75
makabaka163 <17636585435@163.com>
2022-02-26 15:27:23 +0800

    环境修正

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -22,13 +22,7 @@ pub fn sys_yield() -> isize {
     0
 }
 
-pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
+pub fn sys_get_time() -> isize {
     let _us = get_time_us();
-    // unsafe {
-    //     *ts = TimeVal {
-    //         sec: us / 1_000_000,
-    //         usec: us % 1_000_000,
-    //     };
-    // }
-    0
+    _us as isize
 }
